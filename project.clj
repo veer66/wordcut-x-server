d81f414d12eb356ebc36b036efe46a5c8b490632
Vee Satayamas <vsatayamas@gmail.com>
2018-05-08 20:15:30 +0700

    usable

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
+                 [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.3.1"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler rockers.veer66.wordcut-x-server.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [rockers.veer66/wordcut-x "1.0-SNAPSHOT"]]}})
--- src/rockers/veer66/wordcut_x_server/handler.clj
+++ src/rockers/veer66/wordcut_x_server/handler.clj
@@ -1,11 +1,41 @@
 (ns rockers.veer66.wordcut-x-server.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.handler :as handler]
+            [clojure.data.json :as json]
+            [clojure.string :as str]
+            )
+  (:import rockers.veer66.Wordcut
+           rockers.veer66.EdgeType))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(def dix-url (.toURL (java.net.URI/create "http://file.veer66.rocks/dix/tdict-std.txt")))
+(def wc (Wordcut/fromDixUrl dix-url))
+
+(defn convert-etype [etype]
+  (->> etype
+       .toString
+       str/capitalize))
+
+(defn convert-edge [edge]
+  {:s (.s edge)
+   :e (.e edge)
+   :etype (-> edge
+              .etype
+              convert-etype)})
+
+(defn convert-edges [edges]
+  (map convert-edge edges)) 
+
+(defroutes app
+  (GET "/" [] "wordcut-x")
+  (POST "/wordseg" {body :body}
+    (let [text (:text (json/read-str (slurp body)
+                                     :key-fn keyword))]
+      (json/write-str {:words (.segmentToStrList wc text)})))
+  (POST "/dag" {body :body}
+    (let [text (:text (json/read-str (slurp body)
+                                     :key-fn keyword))]
+      (->> (.buildDag wc text)
+          (map convert-edges)
+          json/write-str)))
   (route/not-found "Not Found"))
-
-(def app
-  (wrap-defaults app-routes site-defaults))
