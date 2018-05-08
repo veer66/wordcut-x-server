6582ce88ae78deb749b63c8b3d2b47aabbd8436c
Vee Satayamas <vsatayamas@gmail.com>
2018-05-08 21:23:38 +0700

    Use wordcut-x wrapper

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/veer66/wordcut-x-server"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [rockers.veer66/wordcut-x-clj "0.1.1-SNAPSHOT"]
                  [compojure "1.6.1"]
                  [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.3.1"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler rockers.veer66.wordcut-x-server.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [rockers.veer66/wordcut-x "1.0-SNAPSHOT"]]}})
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
--- src/rockers/veer66/wordcut_x_server/handler.clj
+++ src/rockers/veer66/wordcut_x_server/handler.clj
@@ -4,12 +4,12 @@
             [compojure.handler :as handler]
             [clojure.data.json :as json]
             [clojure.string :as str]
+            [rockers.veer66.wordcut_x_clj.wordcut :refer [create-wordcut-from-url wordseg build-dag]]
             )
   (:import rockers.veer66.Wordcut
            rockers.veer66.EdgeType))
 
-(def dix-url (.toURL (java.net.URI/create "http://file.veer66.rocks/dix/tdict-std.txt")))
-(def wc (Wordcut/fromDixUrl dix-url))
+(def wc (create-wordcut-from-url "http://file.veer66.rocks/dix/tdict-std.txt"))
 
 (defn convert-etype [etype]
   (->> etype
@@ -31,11 +31,9 @@
   (POST "/wordseg" {body :body}
     (let [text (:text (json/read-str (slurp body)
                                      :key-fn keyword))]
-      (json/write-str {:words (.segmentToStrList wc text)})))
+      (json/write-str {:words (wordseg wc text)})))
   (POST "/dag" {body :body}
     (let [text (:text (json/read-str (slurp body)
                                      :key-fn keyword))]
-      (->> (.buildDag wc text)
-          (map convert-edges)
-          json/write-str)))
+      (json/write-str (build-dag wc text))))
   (route/not-found "Not Found"))
